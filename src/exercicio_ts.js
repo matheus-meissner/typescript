8cb890ba3ef4e31eef90d73a1edf04308a088978
Matheus Meissner <matheus.iembo@gmail.com>
2024-07-03 09:40:38 -0300

    exercicio_ts

---
+++ src/exercicio_ts.js
@@ -0,0 +1,10 @@
+"use strict";
+// Função de multiplicação
+const multiplica = (numero1, numero2) => numero1 * numero2;
+let numero1 = 5;
+let numero2 = 2;
+// Função de saudação
+const saudacao = (nome1) => `Olá ${nome1}`;
+let nome1 = 'Matheus';
+console.log(multiplica(numero1, numero2));
+console.log(saudacao(nome1));
---
+++ src/exercicio_ts.ts
@@ -0,0 +1,12 @@
+// Função de multiplicação
+const multiplica = (numero1: number, numero2: number): number => numero1 * numero2
+let numero1: number = 5;
+let numero2: number = 2;
+
+
+// Função de saudação
+const saudacao = (nome1: string): string => `Olá ${nome1}`;
+let nome1: string = 'Matheus';
+
+console.log(multiplica(numero1, numero2));
+console.log(saudacao(nome1));
